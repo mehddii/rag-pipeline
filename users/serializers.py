3bc7f0055f5dfc44765fc46933115381b3bbe5ce
mehdi <mehdi7bs@gmail.com>
2025-05-04 23:25:49 +0100

    Migrate to CustomRegisterSerializer with enhanced validation

--- users/serializers.py
+++ users/serializers.py
@@ -1,71 +1,49 @@
-from rest_framework import serializers
 from django.utils.translation import gettext_lazy as _
-from django.core.exceptions import ValidationError as DjangoValidationError
+from rest_framework import serializers
+from dj_rest_auth.registration.serializers import RegisterSerializer
+import regex as re
 
-from allauth.account import app_settings as allauth_account_settings
-from allauth.account.adapter import get_adapter
-from allauth.account.utils import setup_user_email
-from allauth.utils import get_username_max_length
-from allauth.socialaccount.models import EmailAddress
+NAME_REGEX = re.compile(r"^[\p{L}'][\p{L} '-]*[\p{L}']$", re.UNICODE)
 
-class RegisterSerializer(serializers.Serializer):
-    first_name = serializers.CharField(required=False)
-    last_name = serializers.CharField(required=False)
-    username = serializers.CharField(
-        max_length=get_username_max_length(),
-        min_length=allauth_account_settings.USERNAME_MIN_LENGTH,
-        required=allauth_account_settings.USERNAME_REQUIRED,
+class CustomRegisterSerializer(RegisterSerializer):
+    first_name = serializers.CharField(
+        label=_("first name"),
+        required=True,
+        min_length=1,
+        max_length=150,
+        help_text=_("Letters, apostrophes, hyphens and spaces only")
     )
-    email = serializers.EmailField(required=allauth_account_settings.EMAIL_REQUIRED)
-    password1 = serializers.CharField(write_only=True)
-    password2 = serializers.CharField(write_only=True)
-
-    def validate_username(self, username):
-        username = get_adapter().clean_username(username)
-        return username
-
-    def validate_email(self, email):
-        email = get_adapter().clean_email(email)
-        if allauth_account_settings.UNIQUE_EMAIL:
-            if email and EmailAddress.objects.is_verified(email):
-                raise serializers.ValidationError(
-                    _('A user is already registered with this e-mail address.'),
-                )
-        return email
-
-    def validate_password1(self, password):
-        return get_adapter().clean_password(password)
+    last_name = serializers.CharField(
+        label=_("last name"),
+        required=True,
+        min_length=1,
+        max_length=150,
+        help_text=_("Letters, apostrophes, hyphens and spaces only")
+    )
 
     def validate(self, data):
-        if data['password1'] != data['password2']:
-            raise serializers.ValidationError(_("The two password fields didn't match."))
+        data = super().validate(data)
+        first_name = data.get("first_name", "")
+        last_name = data.get("last_name", "")
+
+        error_msg = _("Allowed: letters, apostrophes ('), hyphens (-), spaces")
+        
+        if not NAME_REGEX.fullmatch(first_name):
+            raise serializers.ValidationError({
+                "first_name": [error_msg]
+            })
+
+        if not NAME_REGEX.fullmatch(last_name):
+            raise serializers.ValidationError({
+                "last_name": [error_msg]
+            })
+
         return data
 
-    def custom_signup(self, request, user):
-        pass
-
     def get_cleaned_data(self):
-        return {
-            "first_name": self.validated_data.get("first_name", ""),
-            "last_name": self.validated_data.get("last_name", ""),
-            "username": self.validated_data.get("username", ""),
-            "password1": self.validated_data.get("password1", ""),
-            "email": self.validated_data.get("email", "")
-        }
-
-    def save(self, request):
-        adapter = get_adapter()
-        user = adapter.new_user(request)
-        self.cleaned_data = self.get_cleaned_data()
-        user = adapter.save_user(request, user, self, commit=False)
-        if "password1" in self.cleaned_data:
-            try:
-                adapter.clean_password(self.cleaned_data['password1'], user=user)
-            except DjangoValidationError as exc:
-                raise serializers.ValidationError(
-                    detail=serializers.as_serializer_error(exc)
-                )
-        user.save()
-        self.custom_signup(request, user)
-        setup_user_email(request, user, [])
-        return user
+        data = super().get_cleaned_data()
+        data.update({
+            "first_name": self.validated_data.get("first_name"),
+            "last_name": self.validated_data.get("last_name")
+        })
+        return data
